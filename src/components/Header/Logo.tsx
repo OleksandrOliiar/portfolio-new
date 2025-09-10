facd91f29f6083f529d22fe4c0f9d9c43008f8f1
oliiaroleksandr <sashaoliyar@gmail.com>
2025-09-10 23:44:38 +0300

    Update dependencies and enhance styles
    - Added `motion` and `rough-notation` as new dependencies in `package.json` for animation and annotation features.
    - Updated `globals.css` with new styles for headings and scrollbar customization.
    - Modified `Header` and `Logo` components to improve structure and integrate the new `Highlighter` component for enhanced visual effects.
    - Introduced `Highlighter` component for customizable text annotations with various actions and colors.

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,7 +1,12 @@
-const Logo = () => {
-  return (
-    <div>Logo</div>
-  )
-}
+import Link from "next/link";
+import { Highlighter } from "@/components/ui/highlighter";
 
-export default Logo
+export default function Logo() {
+  return (
+    <Link href="#home">
+      <Highlighter action="underline" color="hsl(13.2143 73.0435% 54.9020%)">
+        <h3 className="h3 italic tracking-wider">Portfolio</h3>
+      </Highlighter>
+    </Link>
+  );
+}
